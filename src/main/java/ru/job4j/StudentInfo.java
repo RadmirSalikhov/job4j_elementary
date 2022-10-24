3749dab8015aefde491a61755bfaea59a021f203
Radmir <radmir.sf.02@gmail.com>
2022-10-24 14:43:04 +0300

    4.1. Разбор первой программы. [#159033]

--- src/main/java/ru/job4j/StudentInfo.java
+++ src/main/java/ru/job4j/StudentInfo.java
@@ -2,6 +2,7 @@ package ru.job4j;
 
 public class StudentInfo {
     public static void main(String[] args) {
-        System.out.println ("Radmir Salikhov")
+        System.out.println("Radmir Salikhov");
+        System.out.println("13.04.1988");
     }
 }
