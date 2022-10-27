90cf293ad29b19acdfc2be556f7a9c2b02cc5f0b
Radmir <radmir.sf.02@gmail.com>
2022-10-27 08:46:55 +0300

    2. Переменные. [#172635 #355258]

--- src/main/java/ru/job4j/Variables.java
+++ src/main/java/ru/job4j/Variables.java
@@ -2,21 +2,11 @@ package ru.job4j;
 
 public class Variables {
     public static void main(String[] args) {
-        byte age = 34;
+        int age = 18;
         System.out.println(age);
-        char floor = 'm';
-        System.out.println(floor);
-        float growth =  1.76f;
-        System.out.println(growth);
-        short bricks = 27600;
-        System.out.println(bricks);
-        double bacteria =  7000000;
-        System.out.println(bacteria);
-        int distancemoon =  384400;
-        System.out.println(distancemoon);
-        boolean workpermit = true;
-        System.out.println(workpermit);
-        long filesize = 92233720365l;
-        System.out.println(filesize);
+        float length = 78.0332f;
+        System.out.println(length);
+        boolean baby = true;
+        System.out.println(baby);
     }
 }
---
+++ src/main/java/ru/job4j/Variables2.java
@@ -0,0 +1,23 @@
+package ru.job4j;
+
+public class Variables2 {
+    public static void main(String[] args) {
+        byte age = 34;
+        System.out.println(age);
+        char floor = 'm';
+        System.out.println(floor);
+        float growth =  1.76f;
+        System.out.println(growth);
+        short bricks = 27600;
+        System.out.println(bricks);
+        double bacteria =  7000000;
+        System.out.println(bacteria);
+        int distancemoon =  384400;
+        System.out.println(distancemoon);
+        boolean workpermit = true;
+        System.out.println(workpermit);
+        long filesize = 92233720365l;
+        System.out.println(filesize);
+    }
+
+}
