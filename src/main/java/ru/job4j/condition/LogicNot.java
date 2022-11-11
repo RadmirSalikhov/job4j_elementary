7f9a5b46709a29673c42b5bcb3cbc0344f00328c
Radmir <radmir.sf.02@gmail.com>
2022-11-11 20:13:01 +0300

    4.3.3. Логическое отрицание ! [#334072]

---
+++ src/main/java/ru/job4j/condition/LogicNot.java
@@ -0,0 +1,50 @@
+package ru.job4j.condition;
+
+import java.util.Scanner;
+
+import static java.lang.Short.parseShort;
+
+public class LogicNot {
+
+    public static boolean isEven(int num) {
+        return num % 2 == 0;
+    }
+
+    public static boolean isPositive(int num) {
+        return num > 0;
+    }
+
+    public static boolean notEven(int num) {
+        return num % 2 != 0;
+    }
+
+    public static boolean notPositive(int num) {
+        return num < 0;
+    }
+
+    public static boolean notEvenAndPositive(int num) {
+        return num % 2 != 0 && num > 0;
+    }
+
+    public static boolean evenOrNotPositive(int num) {
+        return num % 2 == 0 || num < 0;
+    }
+
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+        System.out.println("Введите число:");
+        int num = parseShort(sc.nextLine());
+        boolean isEven = LogicNot.isEven(num);
+        System.out.println(num + " четное? " + isEven);
+        boolean isPositive = LogicNot.isPositive(num);
+        System.out.println(num + " > 0 ? " + isPositive);
+        boolean notEven = LogicNot.notEven(num);
+        System.out.println(num + " нечетное? " + notEven);
+        boolean notPositive = LogicNot.notPositive(num);
+        System.out.println(num + " <0 ?" + notPositive);
+        boolean notEvenAndPositive = LogicNot.notEvenAndPositive(num);
+        System.out.println(num + " нечетное и >0 ?" + notEvenAndPositive);
+        boolean evenOrNotPositive = LogicNot.evenOrNotPositive(num);
+        System.out.println(num + " четное и <0 ? " + evenOrNotPositive);
+    }
+}
