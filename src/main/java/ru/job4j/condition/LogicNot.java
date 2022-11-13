1879f7dad2133d992240f9e159caf6a64ad0aecc
Radmir <radmir.sf.02@gmail.com>
2022-11-13 10:41:34 +0300

    Правки мелкие

--- src/main/java/ru/job4j/condition/LogicNot.java
+++ src/main/java/ru/job4j/condition/LogicNot.java
@@ -11,27 +11,22 @@ public class LogicNot {
     }
 
     public static boolean isPositive(int num) {
-
         return num > 0;
     }
 
     public static boolean notEven(int num) {
-
         return num % 2 != 0;
     }
 
     public static boolean notPositive(int num) {
-
         return num < 0;
     }
 
     public static boolean notEvenAndPositive(int num) {
-
         return notEven(num) && isPositive(num);
     }
 
     public static boolean evenOrNotPositive(int num) {
-
         return isEven(num) || notPositive(num);
     }
 
