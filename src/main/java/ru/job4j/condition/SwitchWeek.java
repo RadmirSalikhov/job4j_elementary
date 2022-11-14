2009afa3d76f1cf7d53bdddaaefc8bca0885d02b
Radmir <radmir.sf.02@gmail.com>
2022-11-14 19:21:46 +0300

    4.6.1. Switch [#334070]

---
+++ src/main/java/ru/job4j/condition/SwitchWeek.java
@@ -0,0 +1,34 @@
+package ru.job4j.condition;
+
+public class SwitchWeek {
+    public static String nameOfDay(int day) {
+        String dayweek;
+        switch (day) {
+            case 1 :
+                dayweek ="Понедельник";
+                break;
+            case 2 :
+                dayweek ="Вторник";
+                break;
+            case 3 :
+                dayweek ="Среда";
+                break;
+            case 4 :
+                dayweek ="Четверг";
+                break;
+            case 5 :
+                dayweek ="Пятница";
+                break;
+            case 6 :
+                dayweek ="Суббота";
+                break;
+            case 7 :
+                dayweek ="Воскресенье";
+                break;
+            default:
+                dayweek ="Ошибка";
+                break;
+        }
+        return dayweek;
+    }
+}
