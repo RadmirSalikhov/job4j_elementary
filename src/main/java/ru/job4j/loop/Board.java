470931d8d9b4c9d8ed27ab1ed375d52a72bc5fb7
Radmir <radmir.sf.02@gmail.com>
2022-11-15 19:57:39 +0300

    5.3. Построить шахматную доску в псевдографике. [#13559]

---
+++ src/main/java/ru/job4j/loop/Board.java
@@ -0,0 +1,23 @@
+package ru.job4j.loop;
+
+public class Board {
+    public static void paint(int width, int height) {
+        for (int i = 0; i < height; i++) {
+            for (int y = 0; y < width; y++ ) {
+                int sum = i+y;
+                if (sum % 2 == 0) {
+                    System.out.print("X");
+                } else {
+                    System.out.print(" ");
+                }
+            }
+            System.out.println();
+        }
+    }
+
+    public static void main(String[] args) {
+        paint(3, 3);
+        System.out.println();
+        paint(4, 4);
+    }
+}
