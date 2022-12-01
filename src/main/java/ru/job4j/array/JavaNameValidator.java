4cd9def0d48ae9cf34b79b01ad7571548713f712
Radmir <radmir.sf.02@gmail.com>
2022-12-01 10:16:01 +0300

    6.9. String - это массив [#504884]

--- src/main/java/ru/job4j/array/JavaNameValidator.java
+++ src/main/java/ru/job4j/array/JavaNameValidator.java
@@ -6,15 +6,14 @@ import static java.lang.Character.isUpperCase;
 public class JavaNameValidator {
     public static boolean isNameValid(String name) {
         boolean valid = !name.isEmpty() && !isUpperCase(name.codePointAt(0)) && !isDigit(name.codePointAt(0));
-        if (name.isEmpty() || isUpperCase(name.codePointAt(0)) || isDigit(name.codePointAt(0))) {
-            for (int i = 0; i < name.length(); i++) {
-                int code = name.codePointAt(i);
-                if (!isSpecialSymbol(code) || isUpperLatinLetter(code) || isLowerLatinLetter(code)) {
-                    valid = false;
-                    break;
-                }
+        for (int i = 0; i < name.length(); i++) {
+            int code = name.codePointAt(i);
+            if (!isSpecialSymbol(code) && !isUpperLatinLetter(code) && !isLowerLatinLetter(code) && !isDigit(code)) {
+                valid = false;
+                break;
             }
         }
+
         return valid;
     }
 
@@ -23,10 +22,10 @@ public class JavaNameValidator {
     }
 
     public static boolean isUpperLatinLetter(int code) {
-        return code < 65 || code > 90;
+        return code >= 65 || code <= 90;
     }
 
     public static boolean isLowerLatinLetter(int code) {
-        return code < 97 || code > 122;
+        return code >= 97 || code <= 122;
     }
 }
