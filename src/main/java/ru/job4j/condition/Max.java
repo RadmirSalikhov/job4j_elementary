2c00feff5f6becddbae2f845a3337ce3bf329b3e
RadmirSalikhov <radmir.sf.02@gmail.com>
2022-12-16 18:48:53 +0300

    3. Перегрузить метод max для трех чисел [#122644]

--- src/main/java/ru/job4j/condition/Max.java
+++ src/main/java/ru/job4j/condition/Max.java
@@ -3,11 +3,19 @@ package ru.job4j.condition;
 public class Max {
 
     public static int max(int first, int second) {
-        return first > second ? first : second;
+        return Math.max(first, second);
+    }
+
+    public static int max(int first, int second, int third) {
+        return max(first, max(second, third));
+    }
+
+    public static int max(int first, int second, int third, int fourth) {
+        return max(first, max(second, max(third, fourth)));
     }
 
     public static void main(String[] args) {
-        int result = Max.max(3, 6);
+        int result = Max.max(3, 6, 8, 19);
         System.out.println(result);
     }
 }
--- src/test/java/ru/job4j/condition/MaxTest.java
+++ src/test/java/ru/job4j/condition/MaxTest.java
@@ -7,28 +7,31 @@ import static org.assertj.core.api.Assertions.assertThat;
 public class MaxTest {
     @Test
     public void whenMax1To2Then2() {
-        int left = 1;
-        int right = 2;
-        int result = Max.max(left, right);
+        int first = 1;
+        int second = 2;
+        int result = Max.max(first, second);
         int expected = 2;
         assertThat(result).isEqualTo(expected);
     }
 
     @Test
-    public void whenMax3To6Then6() {
-        int left = 3;
-        int right = 6;
-        int result = Max.max(left, right);
-        int expected = 6;
+    public void whenMax3To6To8Then8() {
+        int first = 3;
+        int second = 6;
+        int third = 8;
+        int result = Max.max(first, second, third);
+        int expected = 8;
         assertThat(result).isEqualTo(expected);
     }
 
     @Test
-    public void whenMax3To3Then6() {
-        int left = 3;
-        int right = 3;
-        int result = Max.max(left, right);
-        int expected = 3;
+    public void whenMax3To16To8To9Then16() {
+        int first = 3;
+        int second = 16;
+        int third = 8;
+        int fourth = 9;
+        int result = Max.max(first, second, third, fourth);
+        int expected = 16;
         assertThat(result).isEqualTo(expected);
     }
 }
